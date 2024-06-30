1bbb1ce2e5670703b34517df57f8b0b3beae1f85
Oskari Aleksejev <oskari.aleksejev@tuni.fi>
2024-06-30 23:59:31 +0300

    uibuild

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const errorHandler = (error,request,response,next) => {
     if(error.name === 'CastError'){
         return response.status(400).send({error: 'malformatted id'})
     }else if(error.message === 'name missing'){
-        return response.status(400).json({error: 'name missing'})
+        return response.status(400).send({error: 'name missing'})
     }else if (error.message === 'number missing'){
-        return response.status(400).json({error: 'number missing'})
+        return response.status(400).send({error: 'number missing'})
     }
 
     next(error)
@@ -88,14 +88,10 @@ app.get('/info',(request,response) =>{
 })
 
 //get person w/ id
-app.get('/api/persons/:id',(request,response) =>{
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if(person){
-        response.json(person)
-    }else{
-        response.status(404).end()
-    }
+app.get('/api/persons/:id',(request,response,next) =>{
+    Person.findById(request.params.id).then(person =>{
+            response.json(person)
+    }).catch(error => next(error))
 })
 
 //delete person
@@ -139,7 +135,7 @@ app.put('/api/persons/:id',(request,response,next) =>{
 
     Person.findByIdAndUpdate(request.params.id,person,{new : true}).then(updatedPerson =>{
         response.json(updatedPerson)
-    }).catch(error => next(error))
+    }).catch(error =>next(error))
 })
 
 app.use(errorHandler)
