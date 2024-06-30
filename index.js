b92c767a1a86c07046c1f48f87bee9524412a542
Oskari Aleksejev <oskari.aleksejev@tuni.fi>
2024-06-30 23:41:46 +0300

    uibuild

--- index.js
+++ index.js
@@ -130,6 +130,18 @@ app.post('/api/persons',(request,response,next) =>{
     }).catch(error => next(error))
 })
 
+app.put('/api/persons/:id',(request,response,next) =>{
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id,person,{new : true}).then(updatedPerson =>{
+        response.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
